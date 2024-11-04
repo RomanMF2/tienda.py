773f73012e3f6343ed65633d8c6814e31092bf1a
RomanMF2 <maurokazuya458@gmail.com>
2024-11-04 10:39:13 -0300

    Update and rename Ejercicio-1.txt to Ejercicio-1.py

--- Ejercicio-1.py
+++ Ejercicio-1.py
@@ -1,5 +1,5 @@
   while True:
-        print('Por favor, introduzca una puntuación en una escala de 1 a 5')
+        print('Por favor, introduzca una puntuaciÃ³n en una escala de 1 a 5')
         point = input()
         
         if point.isdecimal():
@@ -15,7 +15,7 @@
                     file_pc.write(f'{post}\n')
                 break
         else:
-            print('Por favor, introduzca la puntuación en números')
+            print('Por favor, introduzca la puntuaciÃ³n en nÃºmeros')
 
 
 def mostrar_resultados():
@@ -27,7 +27,7 @@ def mostrar_resultados():
 def main():
     while True:
         print('Seleccione el proceso que desea aplicar')
-        print('1: Ingresar puntuación y comentario')
+        print('1: Ingresar puntuaciÃ³n y comentario')
         print('2: Comprueba los resultados obtenidos hasta ahora.')
         print('3: Finalizar')
         num = input()
@@ -42,9 +42,9 @@ def main():
                 print('Finalizando')
                 break  
             else:
-                print('Por favor, introduzca un número del 1 al 3')
+                print('Por favor, introduzca un nÃºmero del 1 al 3')
         else:
-            print('Por favor, introduzca un número del 1 al 3')
+            print('Por favor, introduzca un nÃºmero del 1 al 3')
 
 
 if __name__ == "__main__":
